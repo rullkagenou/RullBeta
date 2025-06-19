d645d81af89b1cf9c616909d126b38fd55b3510c
Rullkagenou <rullsikma@gmail.com>
2025-06-19 07:12:21 +0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Scroll smooth
-document.querySelectorAll('header nav a').forEach(link => {
-  link.onclick = function(e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute('href'))
-      .scrollIntoView({ behavior: 'smooth' });
-  };
-});
